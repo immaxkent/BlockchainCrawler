b730bf03ce390c3229c84c7b15bd96de72316eea
Maximillan Kent <maximillan@nethermind.io>
2023-01-02 23:43:06 +0000

    refactoring

---
+++ Scripts/Crawlers/crawlHistoricalData/evaluateHelper.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+
+const oldSolveInstanceHex =
+  "0x9dfdf7e3e630f506a3dfe38cdbe34e196353364235df33e5a3b588488d9a1e78";
+const newSolveInstanceHex =
+  "0x5038a30b900118d4e513ba62ebd647a96726a6f81b8fda73c21e9da45df5423d";
+
+const evaluateIfWeHavePassedReDeployment = (check, switchoverBlock) => {
+  if (check > switchoverBlock) return true;
+};
+
+const evaluateCurrentSolveInstanceHex = (check, switchoverBlock) => {
+  let solveInstanceHex = "";
+  if (!evaluateIfWeHavePassedReDeployment(check, switchoverBlock)) {
+    solveInstanceHex = oldSolveInstanceHex;
+  } else {
+    solveInstanceHex = newSolveInstanceHex;
+  }
+  return solveInstanceHex;
+};
+
+const returnCurrentLevel = (
+  fromBlock,
+  switchoverBlock,
+  txn,
+  log,
+  web3,
+  mappingData
+) => {
+  let result = "";
+  console.log(
+    "log.blockNumber",
+    log.blockNumber,
+    "switchoverBlock",
+    switchoverBlock
+  );
+  if (!evaluateIfWeHavePassedReDeployment(log.blockNumber, switchoverBlock)) {
+    console.log("ive not passed switchover");
+    let input = txn.data;
+    let input_data = "0x" + input.slice(10);
+    if (log.topics[0] === oldSolveInstanceHex) {
+      let decodedAddress = web3.eth.abi.decodeParameter(
+        "address",
+        String(log.data)
+      );
+      result = mappingData[decodedAddress];
+    } else {
+      let decodedAddress = web3.eth.abi.decodeParameter(
+        "address",
+        String(input_data)
+      );
+      result = mappingData[decodedAddress];
+    }
+  } else {
+    console.log("ive passed redeployment", log.topics[3]);
+    const topicsArray = [{ type: "address", name: "level" }];
+    const topic0Array = web3.eth.abi.decodeParameters(
+      topicsArray,
+      String(log.topics[3])
+    );
+    const newLevelAddress = topic0Array.level;
+    console.log("topic0Array", topic0Array, log.blockNumber);
+    result = newLevelAddress;
+  }
+  return result;
+};
+
+module.exports = {
+  evaluateCurrentSolveInstanceHex,
+  evaluateIfWeHavePassedReDeployment,
+  returnCurrentLevel,
+};
---
+++ Scripts/Crawlers/crawlHistoricalData/filterLogs.js
@@ -0,0 +1,48 @@
+const {
+  evaluateCurrentSolveInstanceHex,
+  returnCurrentLevel,
+} = require("./evaluateHelper.js");
+
+const filterLogs = async (
+  logs,
+  nodeProvider,
+  fromBlock,
+  switchoverBlock,
+  web3,
+  mappingDataPath
+) => {
+  const filteredData = [];
+  for (log of logs) {
+    try {
+      let txn = await nodeProvider.getTransaction(log.transactionHash);
+      let block = await nodeProvider.getBlock(log.blockNumber);
+      const filteredLog = {
+        player: String(txn.from),
+        eventType:
+          String(log.topics[0]) ===
+          evaluateCurrentSolveInstanceHex(fromBlock, switchoverBlock)
+            ? "LevelCompleted"
+            : "InstanceCreated",
+        blockNumber: log.blockNumber,
+        timeStamp: block.timestamp,
+        level: returnCurrentLevel(
+          fromBlock,
+          switchoverBlock,
+          txn,
+          log,
+          web3,
+          mappingDataPath
+        ),
+      };
+      // console.log("filteredLog", filteredLog);
+      console.log("filteredLog.level", filteredLog.level, log.blockNumber);
+      filteredData.push(filteredLog);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return filteredData;
+};
+
+module.exports = filterLogs;
