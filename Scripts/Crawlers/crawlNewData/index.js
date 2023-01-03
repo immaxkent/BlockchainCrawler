23887da7d62b88fd3816a2513d19e528a0696d56
Maximillan Kent <maximillan@nethermind.io>
2023-01-03 00:05:24 +0000

    tidying up

--- Scripts/Crawlers/crawlNewData/index.js
+++ Scripts/Crawlers/crawlNewData/index.js
@@ -4,10 +4,12 @@ const Web3 = require("web3");
 const networks = require("../../utils/networkDetails.json");
 const fetchNetworkData = require("./fetchNetworkData.js");
 const updateLeaderBoard = require("./updateLeaderBoard.js");
+const fetchAndAddAliases = require("./addAliases.js");
 
 const fetchNewDataAndUpdateLeaderBoard = async () => {
   const lastCronDrop = await fetchNetworkData(networks);
   updateLeaderBoard(lastCronDrop);
+  //fetchAndAddAliases();
 };
 
 fetchNewDataAndUpdateLeaderBoard();
